1fc3fedccfeaa846cb2002dd4fedf8c0ca63a36d
Luzian Thomas <luzian.thomas@gmail.com>
2026-02-23 14:53:09 +0100

    Update FTIR_Processing.py

--- FTIR_Processing.py
+++ FTIR_Processing.py
@@ -1,106 +1,129 @@
-## simple processing of time domain infrared signals recorded on a FTIR spectrometer
+# Processing time domain interferograms recorded on a FT-IR spectrometer
 
+# -----------------
+# Imports
+# -----------------
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import find_peaks
 
-filename1 = 'Irdata/BAlk.RIFG.dpt'
-filename2 = 'Irdata/BAlk.SIFG.dpt'
-dataBG = np.loadtxt(filename1, delimiter=',',skiprows=0)   # background time domain signal
-dataS = np.loadtxt(filename2, delimiter=',',skiprows=0)    # sample time domain signal
 
+# -----------------
+# Functions
+# -----------------
+def load_data(filename):
+    return np.loadtxt(filename, delimiter=",")
+    
+def extract_zpd_region(signal, N):
+    ix = np.argmax(signal[:,1])
+    low = int(ix - N/2)
+    high = int(ix + N/2)
+    return signal[low:high,1]
 
-## raw BG data plotted (mirror goes once in both directions -> ZPD crossed twice -> effectively 2 spectra taken)
-fig = plt.figure()
-plt.plot(dataBG[:,0],dataBG[:,1])
-plt.xlabel('time')
-plt.ylabel('signal')
-plt.show()
-fig.savefig('FTIR_timedomain.pdf')
+def extract_and_average_zpd(signal, N):
+    half = len(signal) // 2
+    first_half = signal[:half]
+    second_half = signal[half:]
 
+    ifg1 = extract_zpd_region(first_half, N)
+    ifg2 = extract_zpd_region(second_half, N)
 
-## maximum taken (one of the ZPD) and N points around it plotted
-N = pow(2,14)
-ix = np.argmax(dataBG[:,1])
-lowi = int(ix-N/2)
-upperi = int(ix+N/2-1)
-IFGR = dataBG[lowi:upperi,1]
-IFGS = dataS[lowi:upperi,1]
+    avg_ifg = (ifg1 + ifg2) / 2
+    return avg_ifg, ifg1, ifg2
 
-fig = plt.figure()
-plt.plot(IFGR)
-plt.plot(IFGS)
-plt.xlabel('index')
-plt.ylabel('signal')
-ax = plt.gca()
-ax.set_xlim([8000,8400])
-ax.set_ylim([-0.1,0.1])
-plt.show()
+def compute_spectrum(interferogram, N):
+    return np.abs(np.fft.rfft(interferogram, n=N))
 
+def compute_transmittance(sample, background):
+    return sample / background
 
-## fft of N points around ZPD, absolute of resulting array is symmetric because it is a real signal (only half taken)
-## middle point is spectrometer specific frequency (16707.63 cm^-1 in this case)
-FT_BG = abs(np.fft.fft(IFGR,n=N))[1:int(N/2)]
-FT_S = abs(np.fft.fft(IFGS,n=N))[1:int(N/2)]
+def compute_absorbance(transmittance):
+    return -np.log10(transmittance)
 
-length = len(FT_BG)
-nu = np.linspace(start=0,stop=16707.63,num=length)
+def select_region(nu, spectrum, nu_min, nu_max):
+    mask = (nu >= nu_min) & (nu <= nu_max)
+    return nu[mask], spectrum[mask]
 
-## Intensity spectra bg and sample overlayed
-fig = plt.figure()
-plt.plot(nu,FT_BG)
-plt.plot(nu,FT_S)
-plt.xlabel('wavenumber (cm-1)')
-plt.ylabel('Intensity I')
-ax = plt.gca()
-ax.set_xlim([4000,500])
-#ax.set_ylim([-0.1,0.1])
-plt.show()
-fig.savefig('FTIR_intensity.pdf')
+def detect_peaks(absorbance, prominence_factor=0.1):
+    threshold = max(absorbance) * prominence_factor
+    peaks, _ = find_peaks(absorbance, prominence=threshold)
+    return peaks
 
+def print_peak_list(nu, absorbance, peaks_idx, filename=None):
+    
+    print(f'Peak list:\nWavenumber [cm-1]\tAbsorbance [A]')
+    for d in reversed(peaks_idx):
+        print(f'{nu[d]:.5f}\t{absorbance[d]:.2f}')
+        
+    if filename:
+        with open(filename, "w") as f:
+            f.write("Wavenumber (cm^-1)\tAbsorbance\n")
+            for d in reversed(peaks_idx):
+                f.write(f'{nu[d]:.5f}\t{absorbance[d]:.2f}\n')
 
-## transmission spectrum
-transm = FT_S/FT_BG
+def plot_spectrum(nu, spectrum, peaks=None, ylabel="Intensity", title=None, filename=None):
+    plt.figure(figsize=(8,5))
+    plt.plot(nu, spectrum, label='Signal')
+    if peaks is not None:
+        plt.plot(nu[peaks], spectrum[peaks], 'x', color='red', label='Peaks')
+    plt.xlabel(r"Wavenumber (cm$^{-1}$)")
+    plt.ylabel(ylabel)
+    if title:
+        plt.title(title)
+    plt.gca().invert_xaxis()  # typische Darstellung in FTIR
+    plt.tight_layout()
+    if filename:
+        plt.savefig(filename)
+    plt.show()
 
+# -----------------
+# Main logic
+# -----------------
+def main():
+    # Parameter
+    N = 2**14   # Points for fft
+    nu_nyq = 16707.63   # Nyquist frequency of the device (cm-1)
+    nu_min = 400   # Minimum frequency (cm-1) for peak picking and plot
+    nu_max = 4000   # Maximum frequency (cm-1) for peak picking and plot
+    prominence = 0.03  # Prominence factor for prominence based peak detection
+    
+    # load data from files
+    bg = load_data("IRData/BAlk.RIFG.dpt")
+    sample = load_data("IRData/BAlk.SIFG.dpt")
+    
+    # Selection of region around ZPD and averaging
+    bg_ifg_avg, _, _ = extract_and_average_zpd(bg, N)
+    sample_ifg_avg, _, _ = extract_and_average_zpd(sample, N)
 
-fig = plt.figure()
-plt.plot(nu,100*transm)
-plt.xlabel('wavenumber (cm-1)')
-plt.ylabel('%Transmission')
-ax = plt.gca()
-ax.set_xlim([4000,500])
-ax.set_ylim([0,100])
-plt.show()
-fig.savefig('FTIR_transmission.pdf')
+    # fft
+    bg_spec = compute_spectrum(bg_ifg_avg, N)
+    sample_spec = compute_spectrum(sample_ifg_avg, N)
 
+    # frequency axis
+    nu = np.linspace(0, nu_nyq, len(bg_spec))
 
+    # Transmittance and Absorbance
+    transmittance = compute_transmittance(sample_spec, bg_spec)
+    absorbance = compute_absorbance(transmittance)
 
-## absorbance spectrum
+    # Selects the relevant region (typically 400 to 4000 cm-1)
+    nu_region, transmittance_region = select_region(nu, transmittance, nu_min, nu_max)
+    nu_region, absorbance_region = select_region(nu, absorbance, nu_min, nu_max)
 
-absorb = -np.log10(transm)
+    # Peak picking
+    peaks_idx = detect_peaks(absorbance_region, prominence_factor=prominence)
 
-## region for peak picking
-nu_region = nu[np.argmin(abs(nu-500)):np.argmin(abs(nu-4000))]
-absorb_region = absorb[np.argmin(abs(nu-500)):np.argmin(abs(nu-4000))]
-## peak picking based on prominence
-peaks_idx = find_peaks(absorb_region, prominence=max(absorb_region)*0.1)
+    # Plots
+    plot_spectrum(nu_region, transmittance_region*100, ylabel="Transmittance (%T)", title="Transmission Spectrum")
+    plot_spectrum(nu_region, absorbance_region, peaks_idx, ylabel="Absorbance (A)", title="Absorbance Spectrum with Peaks")
 
-fig = plt.figure()
-plt.plot(nu_region,absorb_region)
-plt.plot(nu_region[peaks_idx[0]], absorb_region[peaks_idx[0]], 'x', color="red")
-plt.xlabel('Wavenumber (cm-1)')
-plt.ylabel('Absorbance A')
-ax = plt.gca()
-ax.set_xlim([4000,500])
-ax.set_ylim([-0.02,1])
-plt.show()
-fig.savefig('FTIR_absorbance.pdf')
+    # Print peak list to console (and text file if given)
+    print_peak_list(nu_region, absorbance_region, peaks_idx, filename=None)
 
-## peak list
-print(f'peak list:\nnu [cm-1]\tAbsorbance [A]')
-for d in reversed(peaks_idx[0]):
-    print(f'{nu_region[d]:.5f}\t{absorb_region[d]:.2f}')
-
-
-
+    
+# -----------------
+# Entry point
+# -----------------
+if __name__ == "__main__":
+    main()
 
